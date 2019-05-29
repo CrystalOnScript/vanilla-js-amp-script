9e1b45f0b913dea2f0673d15f609242cc41b578c
Crystal Lambert <crystallambert@google.com>
2019-05-29 10:20:09 +0200

    cleaned up logic

--- finished_code/public/js/script.js
+++ finished_code/public/js/script.js
@@ -8,8 +8,7 @@ const specialCheck = document.getElementById("special");
 const lengthCheck = document.getElementById("eight");
 const submitButton = document.getElementById("submitButton");
 
-const passwordChecks = {
-  items: [
+const passwordChecks = [
     {
       checkRegEx: (password) => (password.search(/[a-z]/) >= 0),
       text: lowerCheck
@@ -25,42 +24,37 @@ const passwordChecks = {
     {
       checkRegEx: (password) => (password.search(/[^a-z0-9]/i) >= 0),
       text: specialCheck
-    }]
-}
+    },
+    {
+      checkRegEx: (password) => (password.length >= 8),
+      text: lengthCheck
+    }
+  ]
 
-function initCheckPassword(el) {
+function initCheckPassword(element) {
    // removes checkFail class and replaced with checkPass if check is met
    const checkMet = (listItem) => {
     listItem.classList.remove("checkFail");
     listItem.classList.add("checkPass");
-    return true
   };
   // removes checkPass class and replaced with checkPass if check is deleted
   const checkRemoved = (listItem) => {
     listItem.classList.remove("checkPass");
     listItem.classList.add("checkFail");
-    return false
   }
   const checkPassword = () => {
-    let failed = false;
-    let successTest = passwordChecks.items.filter((item) => {
-      let passed = item.checkRegEx(el.value);
+    let checksMet = 0;
+    let successTest = passwordChecks.forEach((item) => {
+      let passed = item.checkRegEx(element.value);
        // passed logic 
        return passed ? checkMet(item.text) : checkRemoved(item.text);
     });
-    if (successTest.length == 4 && el.value.length >= 8) {
-      // changes length check text color
-      checkMet(lengthCheck);
-      // enables submit button 
-      submitButton.setAttribute("class", "");
-    }
-
   } 
   
     // is called when user types in input
-    el.addEventListener("keyup", checkPassword);
+    element.addEventListener("keyup", checkPassword);
     // is called if user pastes into input 
-    el.addEventListener("change", checkPassword);
+    element.addEventListener("change", checkPassword);
 };
 
 initCheckPassword(passwordBox);
