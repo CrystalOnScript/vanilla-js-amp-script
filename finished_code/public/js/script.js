4f78411cdded33d8fa26896da0fadbfbc70ab711
Crystal Lambert <crystallambert@google.com>
2019-05-31 12:38:55 +0200

    cleaned up logic

--- finished_code/public/js/script.js
+++ finished_code/public/js/script.js
@@ -1,55 +1,42 @@
 console.log("amp-script here");
 
 const passwordBox = document.getElementById("passwordBox");
-const lowerCheck = document.getElementById("lowercase");
-const upperCheck = document.getElementById("capital");
-const numberCheck = document.getElementById("number");
-const specialCheck = document.getElementById("special");
-const lengthCheck = document.getElementById("eight");
-const submitButton = document.getElementById("submitButton");
+
 
 const passwordChecks = [
-    {
-      checkRegEx: (password) => (password.search(/[a-z]/) >= 0),
-      text: lowerCheck
-    },
-    {
-      checkRegEx: (password) => (password.search(/[A-Z]/) >= 0),
-      text: upperCheck
-    },
-    {
-      checkRegEx: (password) => (password.search(/[0-9]/) >= 0),
-      text: numberCheck
-    },
-    {
-      checkRegEx: (password) => (password.search(/[^a-z0-9]/i) >= 0),
-      text: specialCheck
-    },
-    {
-      checkRegEx: (password) => (password.length >= 8),
-      text: lengthCheck
-    }
-  ]
+  {
+    test: (password) => (password.search(/[a-z]/) >= 0),
+    text: "lowercase"
+  },
+  {
+    test: (password) => (password.search(/[A-Z]/) >= 0),
+    text: "capital"
+  },
+  {
+    test: (password) => (password.search(/[0-9]/) >= 0),
+    text: "number"
+  },
+  {
+    test: (password) => (password.search(/[^a-z0-9]/i) >= 0),
+    text: "special"
+  },
+  {
+    test: (password) => (password.length >= 8),
+    text: "eight"
+  }
+]
 
 function initCheckPassword(element) {
-   // removes checkFail class and replaced with checkPass if check is met
-   const checkMet = (listItem) => {
-    listItem.classList.remove("checkFail");
-    listItem.classList.add("checkPass");
-  };
-  // removes checkPass class and replaced with checkPass if check is deleted
-  const checkRemoved = (listItem) => {
-    listItem.classList.remove("checkPass");
-    listItem.classList.add("checkFail");
-  }
   const checkPassword = () => {
     passwordChecks.forEach((item) => {
-      let passed = item.checkRegEx(element.value);
-       // passed logic 
-       passed ? checkMet(item.text) : checkRemoved(item.text);
+      let passed = item.test(element.value);
+      // captures element
+      let checkText = document.getElementById(item.text)
+      console.log(item.text)
+       // passed/fail logic 
+       checkText.classList.toggle('pass', passed)
     });
   } 
-  
     // is called when user types in input
     element.addEventListener("keyup", checkPassword);
     // is called if user pastes into input 
